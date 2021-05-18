6788435b003d1644c6d57bc4d7b98fbe466875b4
Philipp Reinig <82393020+philippreinig@users.noreply.github.com>
2021-05-18 13:16:40 +0200

    Delete exercises/4-scope_timer/build directory

--- exercises/4-scope_timer/build/CMakeFiles/scopetimer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for scopetimer.
-# This may be replaced when dependencies are built.
--- exercises/4-scope_timer/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/philipp/Development/cplusplus/cplusplus/exercises/ScopeTimer
-# Build directory: /home/philipp/Development/cplusplus/cplusplus/exercises/ScopeTimer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
