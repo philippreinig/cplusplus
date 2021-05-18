a7038991c0031cee2514849194a348504baa4bfc
Philipp Reinig <82393020+philippreinig@users.noreply.github.com>
2021-05-18 13:17:44 +0200

    Delete exercises/6-expression_tree/build directory

--- exercises/6-expression_tree/build/CMakeFiles/exptree.hpp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for exptree.hpp.
-# This may be replaced when dependencies are built.
--- exercises/6-expression_tree/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Development/cplusplus/cplusplus/exercises/6-expression_tree
-# Build directory: C:/Development/cplusplus/cplusplus/exercises/6-expression_tree/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
